439ed1dfc8cf2667ec5e11b35b2e2b653bcdeb52
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-23 18:50:27 +0500

    add images

--- starwars/starships/views.py
+++ starwars/starships/views.py
@@ -7,6 +7,19 @@ responce = requests.get(url).json()["results"]
 
 starships_list = responce
 
+starship_images = {
+    "CR90 corvette": "starships/img/CR90 corvette.jpg",
+    "Star Destroyer": "starships/img/star destroyer.jpeg",
+    "Sentinel-class landing craft": "starships/img/sentinel class landing craft.jpg",
+    "Death Star": "starships/img/death star.jpeg",
+    "Millennium Falcon": "starships/img/millenium falcon.jpg",
+    "Y-wing": "starships/img/Y wing.jpeg",
+    "X-wing": "starships/img/X wing.jpg",
+    "TIE Advanced x1": "starships/img/tie advanced x1.jpg",
+    "Executor": "starships/img/executor.jpeg",
+    "Rebel transport": "starships/img/rebel transport.jpeg",
+}
+
 
 def main_page(request):
     return render(
@@ -16,4 +29,9 @@ def main_page(request):
 
 def starship(request, id):
     starship = starships_list[id]
-    return render(request, "starships/starship.html", {'starship': starship})
+    starship['logo'] = starship_images[starship['name']]
+    return render(
+        request,
+        "starships/starship.html",
+        {"starship": starship, "images": starship_images},
+    )
