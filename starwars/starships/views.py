53739e3d71b0b1e3d6f1e7f11bef98d32a452f5b
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-23 15:25:54 +0500

    add main_page.html

--- starwars/starships/views.py
+++ starwars/starships/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def main_page(request):
-    return HttpResponse('starships_list')
+    return render(request, 'starships/main_page.html')
