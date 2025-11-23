4aa716952137059bb4ba7cb9a5b3702434c8d768
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-23 15:30:28 +0500

    add starship.html (sketch)

--- starwars/starships/urls.py
+++ starwars/starships/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page)
+    path('', views.main_page),
+    path('starship/', views.starship)
 ]
--- starwars/starships/views.py
+++ starwars/starships/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'starships/main_page.html')
+
+def starship(request):
+    return render(request, 'starships/starship.html')
