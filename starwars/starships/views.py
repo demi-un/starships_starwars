c3fdb0fa5e57fdb8d1d4e3a50c1e32f677ce22d6
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-23 15:58:24 +0500

    add css

--- starwars/starships/views.py
+++ starwars/starships/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    return render(request, 'starships/main_page.html')
+    return render(request, "starships/main_page.html")
+
 
 def starship(request):
-    return render(request, 'starships/starship.html')
+    return render(request, "starships/starship.html")
